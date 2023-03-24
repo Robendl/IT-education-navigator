df9efe2e807a30a5406d75f20ad664bb5c090732
Robendl <robendelange@gmail.com>
2023-03-24 19:24:17 +0100

    small fix

--- backend/src/main/java/se/rijksoverheid/mapper/Mapper.java
+++ backend/src/main/java/se/rijksoverheid/mapper/Mapper.java
@@ -4,7 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 
 public class Mapper {
-    private static ModelMapper modelMapper;
+    private static final ModelMapper modelMapper;
 
     static {
         modelMapper = new ModelMapper();
