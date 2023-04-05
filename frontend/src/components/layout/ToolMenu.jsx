c7372c772e1a0b2335a2620e576d2f2e2ac76eb4
tijnedens <tijnedens@gmail.com>
2023-04-05 13:14:21 +0200

    added archive tab

--- frontend/src/components/layout/ToolMenu.jsx
+++ frontend/src/components/layout/ToolMenu.jsx
@@ -1,24 +1,28 @@
 import { useContext } from "react";
 import { OverlayContext } from "./PageOverlay/PageOverlay";
 import { UserContext } from "services/AuthService";
+import { useNavigate } from "react-router-dom";
 
 export default function ToolMenu() {
 
   const overlay = useContext(OverlayContext);
   const user = useContext(UserContext);
 
+  const navigate = useNavigate();
+
   return (
     <div className="tool-menu">
       {user.loggedIn && (user.role === "DATA_MANAGER" || user.role === "ADMIN") && <ToolOption name="Nieuw Item" icon="post_add" action={overlay.openAdd}/>}
-      {user.loggedIn && (user.role === "DATA_MANAGER" || user.role === "ADMIN") && <ToolOption name="Archief" icon="inventory_2"/>}
+      {user.loggedIn && <ToolOption name="Overzicht" icon="list" selected/>}
+      {user.loggedIn && (user.role === "DATA_MANAGER" || user.role === "ADMIN") && <ToolOption name="Archief" icon="inventory_2" action={() => navigate("/?archived=1")} />}
       {user.loggedIn && user.role === "ADMIN" && <ToolOption name="Accountbeheer" icon="manage_accounts"/>}
     </div>
   );
 }
 
-function ToolOption({name, icon, action}) {
+function ToolOption({name, icon, action, selected }) {
   return (
-    <button onClick={action}>
+    <button onClick={action} className={selected ? "selected" : ""}>
       <span className="material-symbols-outlined tool-icon">{icon}</span>
       <span className="tool-name">{name}</span>
     </button>
