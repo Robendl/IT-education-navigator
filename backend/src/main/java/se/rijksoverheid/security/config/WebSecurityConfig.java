64a217f6ace2beee13a211fdd5bb367e0c538b18
DanielKnap <dctknap@gmail.com>
2023-04-19 16:51:20 +0200

    Chenged security order

--- backend/src/main/java/se/rijksoverheid/security/config/WebSecurityConfig.java
+++ backend/src/main/java/se/rijksoverheid/security/config/WebSecurityConfig.java
@@ -71,10 +71,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors().and().csrf().disable()
                 .authorizeRequests().antMatchers("/auth/**").permitAll()
+                .antMatchers(HttpMethod.PUT, "/user/**").hasAnyAuthority("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/courses/**").hasAnyAuthority("ADMIN")
                 .antMatchers(HttpMethod.POST, "/courses").hasAnyAuthority("ADMIN", "DATA_MANAGER")
                 .antMatchers(HttpMethod.PUT, "/courses/**").hasAnyAuthority("ADMIN", "DATA_MANAGER")
-                .antMatchers(HttpMethod.PUT, "/user/**").hasAnyAuthority("ADMIN")
                 .anyRequest().authenticated().and()
                 .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
