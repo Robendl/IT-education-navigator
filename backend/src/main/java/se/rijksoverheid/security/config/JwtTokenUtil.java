1359d2628823b570965b4dd7d85fbf4b86ad0e07
Robendl <robendelange@gmail.com>
2023-04-26 12:09:16 +0200

    some work on login cookies

--- backend/src/main/java/se/rijksoverheid/security/config/JwtTokenUtil.java
+++ backend/src/main/java/se/rijksoverheid/security/config/JwtTokenUtil.java
@@ -1,6 +1,5 @@
 package se.rijksoverheid.security.config;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
