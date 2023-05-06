0e92854fe551b4910a1f97cfea669a7436d1be9d
Tijn Edens <tijnedens@gmail.com>
2023-05-06 21:34:08 +0200

    single value filtering

--- frontend/src/components/layout/FilterMenu.jsx
+++ frontend/src/components/layout/FilterMenu.jsx
@@ -1,73 +1,89 @@
-import { useRef } from "react";
+import { propertyTranslations } from "config/translations";
+import { useEffect, useRef, useState } from "react";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import PropertyLoader from "services/PropertyLoader";
+import { useSearchParams } from "react-router-dom";
 
 /* Menu component holding the filters for the result list */
 export default function FilterMenu() {
-  const filters = {
-    "provincie": {
-      "name": "Provincie",
-      "options": {
-        "limburg": {
-          "name": "Limburg",
-          "selected": false
-        },
-        "drenthe": {
-          "name": "Drenthe",
-          "selected": false
-        },
-        "flevoland": {
-          "name": "Flevoland",
-          "selected": false
-        }
-      }
-    },
-    "niveau": {
-      "name": "Niveau",
-      "options": {
-        "hbo": {
-          "name": "HBO",
-          "selected": false
-        },
-        "wo": {
-          "name": "WO",
-          "selected": false
-        }
-      }
-    },
-    "type": {
-      "name": "Type",
-      "options": {
-        "bachelor": {
-          "name": "Bachelor",
-          "selected": false
-        },
-        "associateDegree": {
-          "name": "Associate Degree (AD)",
-          "selected": false
-        },
-        "kenniscentrum": {
-          "name": "Kenniscentrum",
-          "selected": false
-        }
-      }
-    }
-  }
+  const [filters, setFilters] = useState({});
+  
+  useEffect(() => {
+    PropertyLoader.loadProperties().then(results => {
+      setFilters(results);
+    });
+  })
 
   /* FilterMenu body */
   return (
     <div className="filter-menu">
-      {Object.keys(filters).map(key => <Filter name={filters[key].name} filterKey={key} key={key} options={filters[key].options} />)}
+      {Object.keys(filters).map(key => <Filter name={propertyTranslations[key]} filterKey={key} key={key} options={filters[key].options} />)}
     </div>
   );
 }
 
 /* Filter component holding one type of filter with multiple options */
-function Filter({name, optionKey, options}) {
+function Filter({name, filterKey, options}) {
+  const [expanded, setExpanded] = useState(false);
+  const [itemCount, setItemCount] = useState(3);
+  const [unselectedOptions, setUnselectedOptions] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const minItemCount = 3;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  function expandOptions() {
+    setItemCount(Object.keys(options).length);
+    setExpanded(true);
+  }
+
+  function collapseOptions() {
+    setItemCount(minItemCount);
+    setExpanded(false);
+  }
+
+  function setOption(opt) {
+    setSearchParams(prevParams => {
+      prevParams.set(filterKey, opt);
+      return prevParams;
+    })
+  }
+
+  function clearOption() {
+    setSearchParams(prevParams => {
+      prevParams.delete(filterKey);
+      return prevParams;
+    })
+  }
+
+  useEffect(() => {
+    setSelectedOptions(
+      Object.keys(options)
+        .filter(opt => searchParams.get(filterKey) === opt)
+    );
+    setUnselectedOptions(
+      Object.keys(options)
+        .filter(opt => searchParams.get(filterKey) !== opt)
+        .slice(0, itemCount - (searchParams.has(filterKey) ? 1 : 0))
+    );
+  }, [options, itemCount, filterKey, searchParams])
+
   return (
     <div className="filter">
       <h2>{name}</h2>
-      <div className="filter-option-list">
+      <div className={`filter-option-list ${expanded ? "expand" : ""}`}>
         {
-          Object.keys(options).map(opt => <FilterOption name={options[opt].name} key={opt} selected={options[opt].selected} />)
+          selectedOptions.map(opt => <FilterOption name={options[opt].name} key={opt} selected setOption={() => setOption(opt)} clearOption={clearOption} />)
+        }
+        {
+          unselectedOptions.filter(opt => searchParams.get(filterKey) !== opt).map(opt => <FilterOption name={options[opt].name} key={opt} setOption={() => setOption(opt)} clearOption={clearOption} />)
+        }
+        {Object.keys(options).length > 3 && 
+          <div className="filter-expand">
+            {(expanded && 
+              <button className="filter-expand-button" onClick={collapseOptions}><span>Toon minder</span> <ExpandLessIcon /></button> ) || 
+              <button className="filter-expand-button" onClick={expandOptions}><span>Toon meer</span> <ExpandMoreIcon /></button>}
+          </div>
         }
       </div>
     </div>
@@ -75,7 +91,7 @@ function Filter({name, optionKey, options}) {
 }
 
 /* FilterOption component that can be checked or unchecked */
-function FilterOption ({name, selected = false}) {
+function FilterOption ({ name, selected = false, setOption, clearOption }) {
 
   const ref = useRef(null);
 
@@ -84,9 +100,11 @@ function FilterOption ({name, selected = false}) {
     switch (e.target.checked) {
       case true:
         ref.current.classList.add("checked");
+        setOption()
         break;
       default:
         ref.current.classList.remove("checked");
+        clearOption();
         break;
     }
   }
---
+++ frontend/src/services/PropertyLoader.jsx
@@ -0,0 +1,87 @@
+const filters = {
+  "province": {
+    "options": {
+      0: {
+        "name": "Noord-Brabant"
+      },
+      1: {
+        "name": "Utrecht"
+      },
+      2: {
+        "name": "Drenthe"
+      },
+      3: {
+        "name": "Flevoland"
+      },
+      4: {
+        "name": "Friesland"
+      },
+      5: {
+        "name": "Gelderland"
+      },
+      6: {
+        "name": "Groningen"
+      },
+      7: {
+        "name": "Heerlen"
+      },
+      8: {
+        "name": "Limburg"
+      },
+      9: {
+        "name": "Noord-Holland"
+      },
+      10: {
+        "name": "Overijssel"
+      },
+      11: {
+        "name": "Zuid-Holland"
+      },
+      12: {
+        "name": "Other"
+      },
+    }
+  },
+  "level": {
+    "options": {
+      "hbo": {
+        "name": "HBO"
+      },
+      "wo": {
+        "name": "WO"
+      },
+      // "other": {
+      //   "name": "Anders"
+      // }
+    }
+  },
+  "courseType": {
+    "options": {
+      "bachelor": {
+        "name": "Bachelor"
+      },
+      "associateDegree": {
+        "name": "Associate Degree (AD)"
+      },
+      "kenniscentrum": {
+        "name": "Kenniscentrum"
+      },
+      "master": {
+        "name": "Master"
+      }
+    }
+  }
+};
+
+/* Placeholder function for retrieving all properties with their possible values */
+function loadProperties() {
+  return new Promise((resolve, reject) => {
+    resolve(filters);
+  });
+};
+
+const PropertyLoader = {
+  loadProperties
+};
+
+export default PropertyLoader;
