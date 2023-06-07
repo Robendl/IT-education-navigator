ca375fd463411ee6bf440fa5674aee806f82b5b0
Robendl <robendelange@gmail.com>
2023-06-07 13:35:38 +0200

    docker-compose so far

---
+++ backend/src/test/java/se/rijksoverheid/dto/CourseRequestDTOTest.java
@@ -0,0 +1,12 @@
+package se.rijksoverheid.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class CourseRequestDTOTest {
+    CourseRequestDTO courseRequestDTO;
+
+    @BeforeEach
+    void setUp() {
+        courseRequestDTO = new CourseRequestDTO();
+    }
+}
