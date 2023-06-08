fe8932d900fb79e31dbb4f88f58c26826b902234
Somentus <somentus@gmail.com>
2023-06-08 18:12:12 +0200

    Add missing BadRequestExceptionHandler

--- backend/src/main/java/se/rijksoverheid/exceptions/WebExceptionHandler.java
+++ backend/src/main/java/se/rijksoverheid/exceptions/WebExceptionHandler.java
@@ -1,6 +1,5 @@
 package se.rijksoverheid.exceptions;
 
-import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.security.SecurityException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -10,6 +9,7 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import se.rijksoverheid.exceptions.webexceptions.BadRequestException;
 import se.rijksoverheid.exceptions.webexceptions.EntityConflictException;
 import se.rijksoverheid.exceptions.webexceptions.NotFoundException;
 
@@ -19,6 +19,11 @@ public class WebExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> handleNotFoundException(Exception e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequestException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(AuthenticationException.class)
