66903b9a6f79c6e8367432c3d13b9dfe13c8b38a
Somentus <somentus@gmail.com>
2023-06-09 14:23:44 +0200

    Add UT-054 testHandleNotFoundException

--- backend/src/test/java/se/rijksoverheid/exceptions/WebExceptionHandlerTest.java
+++ backend/src/test/java/se/rijksoverheid/exceptions/WebExceptionHandlerTest.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import se.rijksoverheid.exceptions.webexceptions.BadRequestException;
 import se.rijksoverheid.exceptions.webexceptions.EntityConflictException;
+import se.rijksoverheid.exceptions.webexceptions.NotFoundException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,5 +30,13 @@ public class WebExceptionHandlerTest {
         EntityConflictException exception = new EntityConflictException(errorMessage);
         ResponseEntity<String> response = handler.handleEntityConflictException(exception);
         assertEquals(ResponseEntity.status(HttpStatus.CONFLICT).body(errorMessage), response);
+    }
+
+    @Test
+    void testHandleNotFoundException() {
+        String errorMessage = "not found";
+        NotFoundException exception = new NotFoundException(errorMessage);
+        ResponseEntity<String> response = handler.handleNotFoundException(exception);
+        assertEquals(ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage), response);
     }
 }
