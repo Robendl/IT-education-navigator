6d42deccad2918308c9d7967fb7665e3b72db0ae
Tijn Edens <tijnedens@gmail.com>
2023-06-09 14:52:45 +0200

    fixed login wrong password error

--- frontend/src/pages/login/LoginPage.jsx
+++ frontend/src/pages/login/LoginPage.jsx
@@ -3,14 +3,14 @@ import AuthService, { errorCodes } from "services/AuthService";
 import { CircularProgress } from "@mui/material";
 import './LoginPage.css'
 import { useState } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 /* Main Login page component */
 export default function LoginPage() {
   return (
     <div className="login">
       <LoginForm />
-      
+
     </div>
   );
 }
@@ -37,12 +37,17 @@ function LoginForm() {
           setErrorMessage("Emailadres / wachtwoord is onjuist");
           setIsSubmitting(false);
           break;
-        
+
+        case errorCodes.ERR_PASSWORD_INVALID:
+          setErrorMessage("Wachtwoord is onjuist");
+          setIsSubmitting(false);
+          break;
+
         case errorCodes.ERR_NETWORK:
           setErrorMessage("Server kan niet worden bereikt");
           setIsSubmitting(false);
           break;
-        
+
         default:
           setErrorMessage("Er is iets misgegaan, probeer aub nog een keer");
           setIsSubmitting(false);
@@ -54,21 +59,21 @@ function LoginForm() {
   /* LoginForm body */
   return (
     <form className="login-form" onSubmit={handleSubmit}>
-        <h2>Opleidingsregister Login</h2>
-        <FormEntry type="email" propertyName="Emailadres" propertyKey="username" required/>
-        <FormEntry type="password" propertyName="Wachtwoord" propertyKey="password" required/>
-        <div>
-          <div className="login-form-footer">
-            <button type="submit">
-              {(isSubmitting &&
-                <CircularProgress className="submit-loading"/>) ||
-                <span>Login</span>
-              }
-            </button>
-            <Link to="/register" className="register-link">Registreer</Link>
-          </div>
-          {errorMessage && <span className="login-error-message">{errorMessage}</span>}
+      <h2>Opleidingsregister Login</h2>
+      <FormEntry type="email" propertyName="Emailadres" propertyKey="username" required />
+      <FormEntry type="password" propertyName="Wachtwoord" propertyKey="password" required />
+      <div>
+        <div className="login-form-footer">
+          <button type="submit">
+            {(isSubmitting &&
+              <CircularProgress className="submit-loading" />) ||
+              <span>Login</span>
+            }
+          </button>
+          <Link to="/register" className="register-link">Registreer</Link>
         </div>
+        {errorMessage && <span className="login-error-message">{errorMessage}</span>}
+      </div>
     </form>
   )
 }
